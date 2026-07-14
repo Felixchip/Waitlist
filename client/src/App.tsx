aad87d8487d576a42a733e38c7f774e08a54620a
Felixchip <xchipgraphics@gmail.com>
2026-07-14 17:12:11 +0100

    Fix SPA routing: add wouter base path and 404.html fallback

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,9 @@
 import { useEffect } from "react";
-import { Switch, Route } from "wouter";
+import { Router, Route } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   useEffect(() => {
@@ -22,7 +13,10 @@ function App() {
   return (
     <TooltipProvider>
       <Toaster />
-      <Router />
+      <Router base="/Waitlist">
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Router>
     </TooltipProvider>
   );
 }
